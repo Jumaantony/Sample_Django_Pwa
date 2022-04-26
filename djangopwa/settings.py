5b1d33579fbdb27fd54630c611612c59f51ae892
Jumaantony <jumaantony840gmail.com>
2022-04-26 14:14:59 +0300

    tweaked settings.py

--- djangopwa/settings.py
+++ djangopwa/settings.py
@@ -61,21 +61,21 @@ PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
     {
-        'src': 'static/images/juma.jpeg',
+        'src': '../static/images/wicked.pngss',
         'sizes': '160x160',
         "purpose": 'any',
     }
 ]
 PWA_APP_ICONS_APPLE = [
     {
-        'src': 'static/images/wicked.png',
+        'src': '../static/images/wicked.png',
         'sizes': '160x160',
         "purpose": 'any',
     }
 ]
 PWA_APP_SPLASH_SCREEN = [
     {
-        'src': 'static/images/juma.jpeg',
+        'src': '../static/images/juma.jpeg',
         'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
     }
 ]
